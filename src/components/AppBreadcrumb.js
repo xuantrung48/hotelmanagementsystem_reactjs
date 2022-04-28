2d09d2499fdb162ad93b982b32456c28ab33ae71
nx_trung <nx_trung@brycen.com.vn>
2022-04-28 15:34:56 +0700

    translate breadcrumb

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom'
 import routes from '../routes'
 
 import { CBreadcrumb, CBreadcrumbItem } from '@coreui/react'
+import { t } from 'i18next'
 
 const AppBreadcrumb = () => {
   const currentLocation = useLocation().pathname
@@ -33,14 +34,14 @@ const AppBreadcrumb = () => {
 
   return (
     <CBreadcrumb className="m-0 ms-2">
-      <CBreadcrumbItem href="/">Home</CBreadcrumbItem>
+      <CBreadcrumbItem href="/">{t('Home')}</CBreadcrumbItem>
       {breadcrumbs.map((breadcrumb, index) => {
         return (
           <CBreadcrumbItem
             {...(breadcrumb.active ? { active: true } : { href: breadcrumb.pathname })}
             key={index}
           >
-            {breadcrumb.name}
+            {t(breadcrumb.name)}
           </CBreadcrumbItem>
         )
       })}
