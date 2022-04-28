2107c86c7dc6162c2899630f5f1bf5b6cce651f6
nx_trung <nx_trung@brycen.com.vn>
2022-04-28 17:13:32 +0700

    update create modal

--- src/views/rooms/Facilities.js
+++ src/views/rooms/Facilities.js
@@ -3,6 +3,9 @@ import CIcon from '@coreui/icons-react'
 import { withTranslation } from 'react-i18next'
 import {
   CButton,
+  CForm,
+  CFormInput,
+  CFormLabel,
   CModal,
   CModalBody,
   CModalFooter,
@@ -18,9 +21,44 @@ import {
 
 import { useState, React } from 'react'
 import { t } from 'i18next'
+import Constants from 'src/common/Constants'
+import APIPath from 'src/common/APIPath'
+import { ApiRequest } from 'src/common/ApiRequest'
 
 const Facilities = () => {
   const [visible, setVisible] = useState(false)
+  const [name, setName] = useState('')
+  const [file, setFile] = useState(null)
+
+  const selectFile = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  const changeName = (e) => {
+    setName(e.target.value)
+  }
+
+  const saveOK = async () => {
+    let formData = new FormData()
+    formData.append('name', name)
+    formData.append('file', file)
+
+    let url = APIPath.facilities
+    let obj = { url: url, method: 'post', formData }
+    let response = await ApiRequest(obj)
+    // setLoading(false)
+    if (response.flag === false) {
+      // setSuccess([])
+      // setError(response.message)
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+    } else {
+      // let status = response.data.message
+      // setSuccess([status])
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+      setName('')
+      setFile(null)
+    }
+  }
 
   return (
     <>
@@ -30,15 +68,28 @@ const Facilities = () => {
 
       <CModal visible={visible} onClose={() => setVisible(false)}>
         <CModalHeader onClose={() => setVisible(false)}>
-          <CModalTitle>Modal title</CModalTitle>
+          <CModalTitle>{t('New Facility')}</CModalTitle>
         </CModalHeader>
-        <CModalBody>Woohoo, you&#39;re reading this text in a modal!</CModalBody>
-        <CModalFooter>
-          <CButton color="secondary" onClick={() => setVisible(false)}>
-            Close
-          </CButton>
-          <CButton color="primary">Save changes</CButton>
-        </CModalFooter>
+        <CForm>
+          <CModalBody>
+            <div className="mb-3">
+              <CFormLabel htmlFor="facilityName">{t('Name')}</CFormLabel>
+              <CFormInput type="name" id="facilityName" onChange={changeName} />
+            </div>
+            <div className="mb-3">
+              <CFormLabel htmlFor="formFile">{t('Image')}</CFormLabel>
+              <CFormInput type="file" id="formFile" onChange={selectFile} />
+            </div>
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="secondary" onClick={() => setVisible(false)}>
+              {t('Close')}
+            </CButton>
+            <CButton color="primary" onClick={saveOK}>
+              {t('Save')}
+            </CButton>
+          </CModalFooter>
+        </CForm>
       </CModal>
 
       <CTable hover>
