88b679b36fde743ed454809955f7aeb282c76b0e
nx_trung <nx_trung@brycen.com.vn>
2022-04-28 17:11:14 +0700

    remove badge

--- src/_nav.js
+++ src/_nav.js
@@ -24,10 +24,6 @@ const _nav = [
     name: t('Dashboard'),
     to: '/dashboard',
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
   },
   {
     component: CNavGroup,
