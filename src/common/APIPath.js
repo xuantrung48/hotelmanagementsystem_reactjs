dfade20ed274ada8becae16c93d49401f094b211
nx_trung <nx_trung@brycen.com.vn>
2022-04-28 17:12:26 +0700

    update apipath

--- src/common/APIPath.js
+++ src/common/APIPath.js
@@ -1,8 +1,8 @@
-const APIPath = {
-  /*Common variables*/
-  lang: localStorage.getItem('i18nextLng'),
+let path = process.env.API_PATH
 
-  APIPath: process.env.API_PATH,
+const APIPath = {
+  path: process.env.API_PATH,
+  facilities: path + '/facilities',
 }
 
 export default APIPath
